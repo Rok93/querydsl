1c69acadf2449d31fbe5877fee23035ad7f91c31
Rok93 <kr.kim@mfort.co.kr>
2022-01-23 19:17:46 +0900

    feat: 초기 프로젝트 셋팅

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,43 @@
+package study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+
+        // 초기화
+        em.flush(); // 1차 캐시에 저장되어있는 쿼리들을 모두 날림!
+        em.clear(); // 영속성 컨텍스트 초기화
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member team -> " + member.getTeam());
+        }
+    }
+}
